acc1c5841458cd2171258ec9c2dee301c9a6060f
KingCol13 <48412633+KingCol13@users.noreply.github.com>
2026-04-05 15:03:25 +0100

    WIP: Finally working QSPI, need to clean up

--- embassy-net-wiznet/src/wiznet_spi_interface/qspi.rs
+++ embassy-net-wiznet/src/wiznet_spi_interface/qspi.rs
@@ -7,7 +7,7 @@ impl<'a> From<WiznetSpiOperation<'a>> for qspi::traits::Operation<'a, u8> {
     fn from(value: WiznetSpiOperation<'a>) -> Self {
         match value {
             WiznetSpiOperation::Read(data) => qspi::traits::Operation::Read(data),
-            WiznetSpiOperation::Write(data)  => qspi::traits::Operation::WriteSingleLine(data),
+            WiznetSpiOperation::Write(data)  => qspi::traits::Operation::Write(data),
             WiznetSpiOperation::WriteSingleLine(data) => qspi::traits::Operation::WriteSingleLine(data),
         }
     }
@@ -21,7 +21,7 @@ impl<T: qspi::traits::QspiDevice> ErrorType for WiznetQspiBus<T> {
 }
 
 impl<T: qspi::traits::QspiDevice> WiznetSpiBus for WiznetQspiBus<T> {
-    const SPI_TYPE: SpiType = SpiType::Single;
+    const SPI_TYPE: SpiType = SpiType::Quad;
 
     async fn transaction<'a, const N: usize>(
         &mut self,
