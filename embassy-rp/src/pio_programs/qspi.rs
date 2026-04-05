2ec6b2dcf2e4eaf4adf102cd4c734378d831dbbe
KingCol13 <48412633+KingCol13@users.noreply.github.com>
2026-04-05 14:50:13 +0100

    WIP: converting to QSPI: drop extra dma transfer

--- embassy-rp/src/pio_programs/qspi.rs
+++ embassy-rp/src/pio_programs/qspi.rs
@@ -440,15 +440,12 @@ impl<'d, PIO: Instance, const SM: usize> Qspi<'d, PIO, SM, Async> {
         self.sm.set_config(&self.cfg);
         self.sm.set_enable(true);
 
-        let (rx, tx) = self.sm.rx_tx();
-
-        let mut rx_ch = self.rx_dma.as_mut().unwrap().reborrow();
-        let rx_transfer = rx.dma_pull_discard::<u8>(&mut rx_ch, buffer.len());
+        let tx = self.sm.tx();
 
         let mut tx_ch = self.tx_dma.as_mut().unwrap().reborrow();
         let tx_transfer = tx.dma_push(&mut tx_ch, buffer, false);
 
-        join(tx_transfer, rx_transfer).await;
+        tx_transfer.await;
         defmt::info!("wrote: {}", &buffer);
 
         Ok(())
